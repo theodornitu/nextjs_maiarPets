e07f1470a412ea1451b0d2b869ad9074b5b6e313
nitutheo <nitu.theodor@gmail.com>
2021-12-31 13:45:34 +0200

    Solved Nav bugs, spaced out pages, separated in css classes, added twitter and discord links top right

--- components/Nav.js
+++ components/Nav.js
@@ -1,29 +1,42 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import navStyles from '../styles/Nav.module.css'
+import { FaTwitterSquare, FaDiscord } from 'react-icons/fa';
 
 const Nav = () => {
   return (
     <nav className={navStyles.nav}>
-      <ul>
-        <li>
+      <logo>
           <Link href='/'>
-            <Image src="/MaiarPet1.png" alt="MaiarPet #1" width={60} height={60} />
+            <Image src="/MaiarPet1.png" alt="MaiarPet #1" width={50} height={50} />
+          </Link>
+        </logo>
+      <menu>
+        <page>
+          <Link href='/'>story</Link>
+          </page>
+        <page>
+          <Link href='/plan'>plan</Link>
+          </page>
+        <page>
+          <Link href='/rarity'>rarity</Link>
+          </page>
+        <page>
+          <Link href='/faq'>faq</Link>
+          </page>
+      </menu>
+      <socials>
+        <li>
+          <Link href='https://twitter.com/maiar_pets'>
+            <FaTwitterSquare />
           </Link>
         </li>
         <li>
-          <Link href='/'>story</Link>
+          <Link href='/'>
+            <FaDiscord />
+          </Link>
         </li>
-        <li>
-          <Link href='/plan'>plan</Link>
-        </li>
-        <li>
-          <Link href='/rarity'>rarity</Link>
-        </li>
-        <li>
-          <Link href='/faq'>faq</Link>
-        </li>
-      </ul>
+      </socials>
     </nav>
   )
 }
