8bf7ea40e792532a03b486a55236a8b0202017bc
nitutheo <nitu.theodor@gmail.com>
2021-12-31 18:01:43 +0200

    Fixed bug for mobile view, changed the stylesheet a bit

--- components/Nav.js
+++ components/Nav.js
@@ -2,42 +2,39 @@ import Link from 'next/link'
 import Image from 'next/image'
 import navStyles from '../styles/Nav.module.css'
 import { FaTwitterSquare, FaDiscord } from 'react-icons/fa';
+// import { NavItem, Navv } from "react-bootstrap";
 
 const Nav = () => {
   return (
-    <nav className={navStyles.nav}>
-      <logo>
-          <Link href='/'>
-            <Image src="/MaiarPet1.png" alt="MaiarPet #1" width={50} height={50} />
-          </Link>
-        </logo>
-      <menu>
-        <page>
-          <Link href='/'>story</Link>
-          </page>
-        <page>
-          <Link href='/plan'>plan</Link>
-          </page>
-        <page>
-          <Link href='/rarity'>rarity</Link>
-          </page>
-        <page>
-          <Link href='/faq'>faq</Link>
-          </page>
-      </menu>
-      <socials>
-        <li>
-          <Link href='https://twitter.com/maiar_pets'>
-            <FaTwitterSquare />
-          </Link>
-        </li>
-        <li>
-          <Link href='/'>
-            <FaDiscord />
-          </Link>
-        </li>
-      </socials>
-    </nav>
+    <header>
+      <nav className={navStyles.nav}>
+        <div className={navStyles.container}>
+          <a className={navStyles.logo}>
+            <Link href='/'>
+              <Image src="/MaiarPet1.png" alt="MaiarPet #1" width={50} height={50} />
+            </Link>
+          </a>
+          <ul className={navStyles.menu}>
+            <li className={navStyles.item}><Link href='/'>story</Link></li>
+            <li className={navStyles.item}><Link href='/plan'>plan</Link></li>
+            <li className={navStyles.item}><Link href='/rarity'>rarity</Link></li>
+            <li className={navStyles.item}><Link href='/faq'>faq</Link></li>
+          </ul>
+          <a className={navStyles.socials}>
+          <li>
+            <Link href='https://twitter.com/maiar_pets'>
+              <FaTwitterSquare />
+            </Link>
+          </li>
+          <li>
+            <Link href='/'>
+              <FaDiscord />
+            </Link>
+          </li>
+          </a>
+        </div>
+      </nav>
+    </header>
   )
 }
 
