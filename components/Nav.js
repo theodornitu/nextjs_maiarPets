f86ae7cff24017eb13c89da67b8a681f57dae9f4
nitutheo <nitu.theodor@gmail.com>
2022-01-03 19:11:25 +0200

    Kidna finished the dropdown menu for mobile view, it's working now

--- components/Nav.js
+++ components/Nav.js
@@ -1,6 +1,3 @@
-// Components import
-import Dropdown from './Dropdown';
-
 // React/Next libs import
 import React, { useState } from 'react';
 import Link from 'next/link'
@@ -20,49 +17,48 @@ const Nav = () => {
 
   return (
     <header>
-      <nav className={click ? navStyles['navActive'] : navStyles['nav']}>
-        <div className={click ? navStyles['containerActive'] : navStyles['container']}>
-          {/* Logo */}
-          <a className={navStyles.logo}>
-            <Link href='/' onClick={closeMobileMenu}>
-              <Image src="/MaiarPet1.png" alt="MaiarPet #1" width={50} height={50} />
-            </Link>
-          </a>
-          <a className={navStyles.pageTitle}>
-            <Link href='/' onClick={closeMobileMenu}>| Maiar Pets</Link>
-          </a>
-          {/* Nav menu */}
-          <ul className={click ? navStyles['menuActive'] : navStyles['menu']}>
-            <li className={navStyles.item}>
-              <Link href='/' onClick={closeMobileMenu}>story</Link>
-            </li>
-            <li className={navStyles.item}>
-              <Link href='/plan' onClick={closeMobileMenu}>plan</Link>
-            </li>
-            <li className={navStyles.item}>
-              <Link href='/rarity' onClick={closeMobileMenu}>rarity</Link>
-            </li>
-            <li className={navStyles.item}>
-                <Link href='/faq' onClick={closeMobileMenu}>faq</Link>
-              </li>
-          </ul>
-          {/* Socials links */}
-          <a className={navStyles.socials}>
-            <li>
-              <Link href='https://twitter.com/maiar_pets'>
-                <FaTwitterSquare />
-              </Link>
-            </li>
-            <li>
-              <Link href='/'>
-                <FaDiscord />
-              </Link>
-            </li>
-            <li className={navStyles['menuIcon']} onClick={handleClick}>
-                <FaBars /> 
-            </li>
-          </a>
+      <nav className={navStyles['nav']}>
+        {/* Logo */}
+        <div className={navStyles.logo}>
+          <Link href='/' onClick={closeMobileMenu}>
+            <Image src="/MaiarPet1.png" alt="MaiarPet #1" width={50} height={50} />
+          </Link>
         </div>
+        {/* Title */}
+        <div className={navStyles.pageTitle}>
+          <Link href='/' onClick={closeMobileMenu}>| Maiar Pets </Link>
+        </div>
+        {/* Socials links */}
+        <div className={navStyles.socials}>
+          <li>
+            <Link href='https://twitter.com/maiar_pets'>
+              <FaTwitterSquare />
+            </Link>
+          </li>
+          <li>
+            <Link href='/'>
+              <FaDiscord />
+            </Link>
+          </li>
+          <li className={navStyles['menuIcon']} onClick={handleClick}>
+            <FaBars /> 
+          </li>
+        </div>
+        {/* Nav menu */}
+        <ul className={click ? navStyles['menuActive'] : navStyles['menu']}>
+          <li className={navStyles['item']}>
+            <Link href='/' onClick={closeMobileMenu}>story</Link>
+          </li>
+          <li className={navStyles['item']}>
+            <Link href='/plan' onClick={closeMobileMenu}>plan</Link>
+          </li>
+          <li className={navStyles['item']}>
+            <Link href='/rarity' onClick={closeMobileMenu}>rarity</Link>
+          </li>
+          <li className={navStyles['item']}>
+            <Link href='/faq' onClick={closeMobileMenu}>faq</Link>
+          </li>
+        </ul>
       </nav>
     </header>
   )
