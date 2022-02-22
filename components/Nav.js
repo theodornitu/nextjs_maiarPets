e8404506d4f12c109835261555bfe492fe0ddddb
florin_mac <mac@mac.mac>
2022-02-23 00:43:58 +0200

    Update menu display.

--- components/Nav.js
+++ components/Nav.js
@@ -19,51 +19,57 @@ const Nav = () => {
 
   return (
     <header onScroll={closeMobileMenu}>
-      <nav className={navStyles['nav']}>
-        {/* Logo */}
-        <div className={navStyles.logo}>
-          <Link href={navPages.index} onClick={closeMobileMenu}>
-            <Image src="/MaiarPet1.png" alt="MaiarPet #1" width={50} height={50} />
-          </Link>
+      <nav className={navStyles.nav}>
+        <div className={navStyles.gridNav}>
+          {/* Logo */}
+          <div className={navStyles.logoTitleContainer}>
+            <div className={navStyles.logoContainer}>
+              <Link href={navPages.index} onClick={closeMobileMenu}>
+                <div className={navStyles.logo}>
+                  <Image src="/MaiarPet1.png" alt="MaiarPet #1" width={50} height={50} />
+                </div>
+              </Link>
+            </div>
+            {/* Title */}
+            <div className={navStyles.pageTitle}>
+              <Link href={navPages.index} onClick={closeMobileMenu}>| Maiar Pets </Link>
+            </div>
+          </div>
+          {/* Nav menu */}
+          <ul className={click ? navStyles.menuActive : navStyles.menu}>
+            <li className={navStyles.item}>
+              <Link href={navPages.index} onClick={closeMobileMenu}>story</Link>
+            </li>
+            <li className={navStyles.item}>
+              <Link href={navPages.plan} onClick={closeMobileMenu}>plan</Link>
+            </li>
+            <li className={navStyles.item}>
+              <Link href={navPages.traits} onClick={closeMobileMenu}>traits</Link>
+            </li>
+            <li className={navStyles.item}>
+              <Link href={navPages.faq} onClick={closeMobileMenu}>faq</Link>
+            </li>
+            <li className={navStyles.itemMint}>
+              <Link href={navPages.home} onClick={closeMobileMenu}>mint</Link>
+            </li>
+          </ul>
+          {/* Socials links */}
+          <ul className={navStyles.socials}>
+            <li>
+              <a target="_blank" href='https://twitter.com/maiar_pets'>
+                <FaTwitterSquare />
+              </a>
+            </li>
+            <li>
+              <a target="_blank" href='https://discord.gg/zCDqHrVEWK'>
+                <FaDiscord />
+              </a>
+            </li>
+            <li className={navStyles['menuIcon']} onClick={handleClick}>
+              <FaBars /> 
+            </li>
+          </ul>
         </div>
-        {/* Title */}
-        <div className={navStyles.pageTitle}>
-          <Link href={navPages.index} onClick={closeMobileMenu}>| Maiar Pets </Link>
-        </div>
-        {/* Socials links */}
-        <div className={navStyles.socials}>
-          <li>
-            <a target="_blank" href='https://twitter.com/maiar_pets'>
-              <FaTwitterSquare />
-            </a>
-          </li>
-          <li>
-            <a target="_blank" href='https://discord.gg/zCDqHrVEWK'>
-              <FaDiscord />
-            </a>
-          </li>
-          <li className={navStyles['menuIcon']} onClick={handleClick}>
-            <FaBars /> 
-          </li>
-        </div>
-        {/* Nav menu */}
-        <ul className={click ? navStyles['menuActive'] : navStyles['menu']}>
-          <li className={navStyles['item']}>
-            <Link href={navPages.index} onClick={closeMobileMenu}>story</Link>
-          </li>
-          <li className={navStyles['item']}>
-            <Link href={navPages.plan} onClick={closeMobileMenu}>plan</Link>
-          </li>
-          <li className={navStyles['item']}>
-            <Link href={navPages.traits} onClick={closeMobileMenu}>traits</Link>
-          </li>
-          <li className={navStyles['item']}>
-            <Link href={navPages.faq} onClick={closeMobileMenu}>faq</Link>
-          </li>
-          <li className={navStyles.itemMint}>
-            <Link href={navPages.home} onClick={closeMobileMenu}>mint</Link>
-          </li>
-        </ul>
       </nav>
     </header>
   )
